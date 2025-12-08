78a312ac14608fa8a2aa1d7fee935ff9d25539a4
leaflore <br.kemp1@gmail.com>
2025-12-08 12:48:34 -0800

    20251208 - Project Development

--- drawing.js
+++ drawing.js
@@ -1,4 +1,4 @@
-function setup() {
+/*function setup() {
     createCanvas(100,  100, SVG)
     background(255)
     fill(150)
@@ -9,4 +9,22 @@ function draw() {
     var r = frameCount % 200 * Math.sqrt(2)
     background(255)
     ellipse(0, 0, r, r)
+}*/
+
+let img
+
+//My exercise to write notes of syntax to eventually memoorize it.
+//Load the image and create :) a p5.Image object.
+function preload() {
+    img = loadImage('assets/20251208_SitePlanMain.svg')
+}
+
+function setup() {
+    createCanvas(1366, 768, SVG)
+    background(255)
+
+    //Draw the image
+    image(img, 0,0)
+
+    describe('Image of the underside of a white umbrella and a gridded ceiling.')
 }
