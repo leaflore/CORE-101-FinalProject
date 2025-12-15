db3efec57737dccc3010741935c4d5d0e527c9dc
leaflore <br.kemp1@gmail.com>
2025-12-15 03:11:58 -0800

    20251215 - Course Development

--- drawing.js
+++ drawing.js
@@ -34,8 +34,8 @@ var distToSchool = []
 var xCenter = []
 var yCenter = []
 var pCoords = []
-var pLineCoords = []
-var plCoordGroups = []
+var plCoordsList = []
+var plcGroupLength = []
 var remapV
 
 var dotPair
@@ -72,19 +72,20 @@ function Remap(){
 
         //Remap function for x
         this.remapX = function(x1){
-            this.funcX1 = x1
+            this.funcX1 = Number(x1)
 
             return this.funcX1
         }
 
         //Remap function for y
         this.remapY = function(y1){
-            this.funcY1 = y1 + 116
+            this.funcY1 = Number(y1) + 116
 
             return this.funcY1
         }
 }
 
+remapV = new Remap()
 
 //Main setup function
 function setup() {
@@ -111,18 +112,30 @@ function setup() {
         yCenter[i] = dataTable.getNum(i, 3)
         pCoords[i] = dataTable.getString(i, 4)
 
-        pLineCoords[i] = split(pCoords[i], ',')
-        plCoordGroups[i] = Math.round((pLineCoords[i].length) / 4)
+        //Process dataset for program manipulation
+        plCoordsList[i] = split(pCoords[i], ',') //Separates the x and y coordinates from the CSV file row
+        plcGroupLength[i] = (Math.round((plCoordsList[i].length) / 4) + 1) //Get amount of points in the distance to nearest grocery store
 
+        //Convert point string into a number
+        for(var j = 0; j < plCoordsList[i].length; j++){
+            plCoordsList[i][j] = Number(plCoordsList[i][j])
+        }
     }
+    
+    console.log(plCoordsList[0])
+    console.log(plcGroupLength[0])
+    console.log(plCoordsList[0][0])
 
-    console.log(pLineCoords[0])
-    console.log(plCoordGroups[0])
-    //Separates the x and y coordinates from the CSV file row
+    qple = 0 * 4
 
+    x1 = remapV.remapX(plCoordsList[0][2])
+    y1 = remapV.remapY(plCoordsList[0][3])
+    x2 = remapV.remapX(plCoordsList[0][4])
+    y2 = remapV.remapY(plCoordsList[0][5])
     
-    
-    
+    console.log(y1)
+
+   
 }
 
 
@@ -148,7 +161,7 @@ function _applyPendingP5Resize() {
     }
 }
 
-remapV = new Remap()
+
  
 
 //Main draw function
@@ -170,18 +183,28 @@ function draw(){
         // The loop will include maybe some sequential part in the loop that is offfset
 
         //Make pLCoordGroups a list for rach array goups
-        
 
-        /*for( var j = 0; j < plCoordGroups; i){
-            line(pLineCoords[i][j], pLineCoords[i][j+1], pLineCoords[i][j+2], pLineCoords[i][j+3])
-        }*/
+        for( var j = 0; j <= plcGroupLength[i]; j++){
+            
+            qple = (2*j)
+
+            x1 = remapV.remapX(plCoordsList[i][qple])
+            y1 = remapV.remapY(plCoordsList[i][qple+1])
+            x2 = remapV.remapX(plCoordsList[i][qple+2])
+            y2 = remapV.remapY(plCoordsList[i][qple+3])
+
+            line(x1, y1, x2, y2)
+
+
+            /*console.log(x1)
+            console.log(y1)
+            console.log(x2)
+            console.log(y2)*/
+        }
+       
     }
-
-
-    
-    
-   
-
+    //line(x1, y1, x2, y2)
     //ellipse(remapV.remapX(674.56), remapV.remapY(327.36), 10, 10)
     //console.log(remapV.remapX(0))
+    
 }
